b8c1da5c2c818e7749db03fd467881872e368a67
Flor1nD <grudkinkostya2005@gmail.com>
2025-10-09 15:55:25 +0300

    feat: init

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,81 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({name, age}) {
+
+
+  const [count, setCount] = useState(0);
+  const [vis, setVis] = useState(true);
+
+  function Counter() {
+    setCount(count + 1);
+  }
+
+  function Visibler() {
+    setVis(!vis);
+  }
+
+
+
+  useEffect(() => {
+    console.log("Хуй")
+  })
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header>
+        
+        <div className='headBlock'>
+          <a href="https://vite.dev" target='_blank' className='hueten'>
+            <p>Ало, {name} {age}?</p> 
+          </a>
+
+
+          <a href="https://react.dev" target='_blank' className='hueten'>
+            <p>Да-да, иди нахуй</p>
+          </a>
+        </div>
+
+
+        
+
+
+
+        <div className='headBlock'>
+          <a href="https://vite.dev" target='_blank' className='hueten'>
+            <strong>имба</strong>
+          </a>
+
+
+          <a href="https://react.dev" target='_blank' className='hueten'>
+            <img src={reactLogo} alt='хуй' className='hueta'></img>
+          </a>  
+        </div>
+
+      </header>
+
+
+      
+      <body>
+        <button onClick={Visibler}>
+
+          {vis ? "Скрыть" : "Показать"}
+          
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        {vis && (
+          <p>Хуета</p>
+        )}
+
+
+      </body>
+
     </>
   )
+
+  
 }
 
 export default App
