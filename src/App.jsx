7970ef81e60f69d5142e0985868bf576170478ab
Flor1nD <grudkinkostya2005@gmail.com>
2025-10-09 19:02:08 +0300

    feat: init

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ function App({name, age}) {
   const [count, setCount] = useState(0);
   const [vis, setVis] = useState(true);
 
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
   function Counter() {
     setCount(count + 1);
   }
@@ -23,33 +19,10 @@ function App({name, age}) {
 
 
   useEffect( () => {
-    const fetchPosts = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
-
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        setPosts(data);
-      }
-
-      catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchPosts();
-  }, []);
+    console.log("пшел нахууууй")
+  });
   
 
-  if (loading) return <div>Загрузка...</div>;
-  if (error) return <div>Ошибка: {error}</div>;
 
   return (
     <>
@@ -97,13 +70,6 @@ function App({name, age}) {
           <p>Хуетень</p>
         )}
 
-        <h1>Посты</h1>
-        {posts.slice(0, 5).map(post => (
-          <div key={post.id} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </div>
-        ))}
       </body>
 
     </>
