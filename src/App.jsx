17e0faae430831a71517166b5ddf2b7574fc8fc4
Flor1nD <grudkinkostya2005@gmail.com>
2025-10-14 11:02:13 +0300

    feat: init

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,36 @@ import './App.css'
 
 function App({name, age}) {
 
-
-  const [count, setCount] = useState(0);
   const [vis, setVis] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  function Counter() {
-    setCount(count + 1);
+
+  const setPost = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+      if (!response.ok) {
+        throw new Error('Ошибка при получении данных');
+      }
+
+      const data = await response.json();
+      setPosts(data);
+    }
+    catch (err) {
+      setError(err.message);
+    }
+    finally {
+      setLoading(false);
+    }
   }
+
+  
+
+  
 
   function Visibler() {
     setVis(!vis);
@@ -35,7 +58,7 @@ function App({name, age}) {
 
 
           <a href="https://react.dev" target='_blank' className='hueten'>
-            <p>Да-да, иди нахуй</p>
+            <p>Да-да, иди нахуйй</p>
           </a>
         </div>
 
@@ -60,14 +83,41 @@ function App({name, age}) {
 
       
       <body>
-        <button onClick={Visibler}>
+        <p>
+          <button onClick={Visibler}>
 
-          {vis ? "Скрыть" : "Показать"}
-          
-        </button>
+            {vis ? "Скрыть" : "Показать"}
+            
+          </button>
+        </p>
 
         {vis && (
           <p>Хуетень</p>
+        )}
+
+
+        <button onClick={setPost} disabled={loading}>
+          {loading ? 'Загрузка...' : 'Загрузить посты'}
+        </button>
+
+        {error && 
+        <div style={{ color: 'red' }}>
+          Ошибка: {error}
+        </div>
+        }
+
+        <h1>Посты ({posts.length})</h1>
+        
+        {posts.length > 0 && 
+        (
+          <div>
+            {posts.slice(0, 8).map(post => (
+              <div key={post.id} style={{ border: '1px solid  #ccc', margin: '10px', padding: '10px' }}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </div>
+            ))}
+          </div>
         )}
 
       </body>
